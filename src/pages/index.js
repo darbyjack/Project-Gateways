3b86858b0372b067a965259ba5cda518af3c8534
darbyjack <admin@glaremasters.me>
2021-04-25 03:04:48 -0500

    Integrated Bootstrap Grid to handle responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PageLayout from "../components/PageLayout"
 import SEO from "../components/SEO"
-import { Container } from "react-bootstrap"
+import {Col, Container, Row} from "react-bootstrap"
 import {Component} from "react";
 
 const PAPER_URL = 'https://papermc.io/api/v2/projects/paper/versions/1.16.5';
@@ -37,46 +37,62 @@ class Index extends Component {
                 <SEO />
                 <Container className="text-center pt-5 mt-5" fluid>
                     <Container className="py-0 my-0 border-light border">
-                        <h1
-                            style={{
-                                fontSize: "5rem",
-                                color: "black",
-                            }}
-                        >
-                            <span className="first-name">Project Gateways</span>
-                        </h1>
+                        <Row>
+                            <Col>
+                                <h1
+                                style={{
+                                    fontSize: "5rem",
+                                    color: "black",
+                                }}
+                            >
+                                <span className="first-name">Project Gateways</span>
+                            </h1>
+                            </Col>
+                        </Row>
                     </Container>
                     <hr className="my-3 w-25" />
-                    <div className="d-md-inline-flex icons-container">
-                        <a href="https://dl.airplane.gg/latest/Airplane-JDK11/launcher-airplane.jar" download>
+                    <Container>
+                        <Row>
+                         <Col>
+                             <a href="https://dl.airplane.gg/latest/Airplane-JDK11/launcher-airplane.jar" download>
                 <span className="icon-airplane icon"><span className="path1"></span><span className="path2"></span><span
                     className="path3"></span><span className="path4"></span><span className="path5"></span><span
                     className="path6"></span><span className="path7"></span><span className="path8"></span><span
                     className="path9"></span><span className="path10"></span><span className="path11"></span><span
                     className="path12"></span></span>
-                        </a>
-                        <a href={`https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/${this.state.paper}/downloads/paper-1.16.5-${this.state.paper}.jar`} download>
+                             </a>
+                         </Col>
+                            <Col>
+                                <a href={`https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/${this.state.paper}/downloads/paper-1.16.5-${this.state.paper}.jar`} download>
                   <span className="icon-paper icon"><span className="path1"></span><span className="path2"></span><span
                       className="path3"></span><span className="path4"></span><span className="path5"></span><span
                       className="path6"></span><span className="path7"></span><span className="path8"></span><span
                       className="path9"></span><span className="path10"></span><span className="path11"></span></span>
-                        </a>
-                        <a href="https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar" download>
+                                </a>
+                            </Col>
+                            <Col>
+                                <a href="https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar" download>
                   <span className="icon-purpur icon"><span className="path1"></span><span className="path2"></span><span
                       className="path3"></span><span className="path4"></span><span className="path5"></span><span
                       className="path6"></span><span className="path7"></span><span className="path8"></span><span
                       className="path9"></span></span>
-                        </a>
-                        <a href="https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/artifact/tuinity-paperclip.jar" download>
+                                </a>
+                            </Col>
+                            <Col>
+                                <a href="https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/artifact/tuinity-paperclip.jar" download>
                   <span className="icon-tuinity icon"><span className="path1"></span><span className="path2"></span><span
                       className="path3"></span><span className="path4"></span><span className="path5"></span><span
                       className="path6"></span><span className="path7"></span><span className="path8"></span></span>
-                        </a>
-                        <a href="https://versions.velocitypowered.com/download/1.1.5.jar" download>
+                                </a>
+                            </Col>
+                            <Col>
+                                <a href="https://versions.velocitypowered.com/download/1.1.5.jar" download>
                   <span className="icon-velocity icon"><span className="path1"></span><span className="path2"></span><span
                       className="path3"></span></span>
-                        </a>
-                    </div>
+                                </a>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Container>
             </PageLayout>
         );
