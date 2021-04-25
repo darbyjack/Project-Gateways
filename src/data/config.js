6fc327d8d0e0d106194776167877253863511407
darbyjack <admin@glaremasters.me>
2021-04-24 22:55:37 -0500

    More Content

---
+++ src/data/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    defaultTitle: 'Project Gateways',
+    logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+    author: 'Glare',
+    url: 'https://glare.dev/gateways',
+    legalName: 'Project Gateways',
+    defaultDescription: 'Easy access to latest versions of projects I care about.',
+};
