5d3e23c43fa992bbbc6dc469e5838973f8f529de
darbyjack <admin@glaremasters.me>
2021-04-25 12:42:23 -0500

    Purpur inherits Airplane. Don't need both.

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,15 +53,6 @@ class Index extends Component {
                     <hr className="my-3 w-25" />
                     <Container>
                         <Row>
-                         <Col>
-                             <a href="https://dl.airplane.gg/latest/Airplane-JDK11/launcher-airplane.jar" download>
-                <span className="icon-airplane icon"><span className="path1"></span><span className="path2"></span><span
-                    className="path3"></span><span className="path4"></span><span className="path5"></span><span
-                    className="path6"></span><span className="path7"></span><span className="path8"></span><span
-                    className="path9"></span><span className="path10"></span><span className="path11"></span><span
-                    className="path12"></span></span>
-                             </a>
-                         </Col>
                             <Col>
                                 <a href={`https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/${this.state.paper}/downloads/paper-1.16.5-${this.state.paper}.jar`} download>
                   <span className="icon-paper icon"><span className="path1"></span><span className="path2"></span><span
@@ -71,18 +62,18 @@ class Index extends Component {
                                 </a>
                             </Col>
                             <Col>
+                                <a href="https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/artifact/tuinity-paperclip.jar" download>
+                  <span className="icon-tuinity icon"><span className="path1"></span><span className="path2"></span><span
+                      className="path3"></span><span className="path4"></span><span className="path5"></span><span
+                      className="path6"></span><span className="path7"></span><span className="path8"></span></span>
+                                </a>
+                            </Col>
+                            <Col>
                                 <a href="https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar" download>
                   <span className="icon-purpur icon"><span className="path1"></span><span className="path2"></span><span
                       className="path3"></span><span className="path4"></span><span className="path5"></span><span
                       className="path6"></span><span className="path7"></span><span className="path8"></span><span
                       className="path9"></span></span>
-                                </a>
-                            </Col>
-                            <Col>
-                                <a href="https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/artifact/tuinity-paperclip.jar" download>
-                  <span className="icon-tuinity icon"><span className="path1"></span><span className="path2"></span><span
-                      className="path3"></span><span className="path4"></span><span className="path5"></span><span
-                      className="path6"></span><span className="path7"></span><span className="path8"></span></span>
                                 </a>
                             </Col>
                             <Col>
